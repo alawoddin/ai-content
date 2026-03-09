c618dd823786adcabf0f1cb9c065569d7537dc37
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-03-09 15:40:29 +0430

    the admin prfile is done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,6 +9,10 @@
     <title>Home - CopyGen - AI Writer &amp; Copywriting Landing Page HTML Template.</title>
     <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
 
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
+
+
 
 </head>
 
@@ -35,8 +39,40 @@
             </div>
         </div>
     </div>
+
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+      
     <script src="{{ asset('backend/assets/js/bundle.js') }}"></script>
     <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
+
+     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break; 
+ }
+ @endif 
+</script>
+
+
+
 </body>
 
 </html>
