7495ab831830b37901f3cc44c14cb99070c560b4
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-03-20 18:44:06 +0430

    the same change is add

--- resources/views/home/home_master.blade.php
+++ resources/views/home/home_master.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="shortcut icon" href="{{ asset('frontend/images/favicon.png') }}">
     <title>EasyGen - AI Writer SaaS Application.</title>
-    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css?v1.5.0') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
 </head>
 
 <body class="nk-body " data-menu-collapse="lg">
@@ -20,8 +20,8 @@
         </main>
         @include('home.body.footer')
     </div>
-    <script src="{{ asset('frontend/assets/js/bundle.js?v1.5.0') }}"></script>
-    <script src="{{ asset('frontend/assets/js/scripts.js?v1.5.0') }}"></script>
+    <script src="{{ asset('frontend/assets/js/bundle.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/scripts.js') }}"></script>
 </body>
 
 </html>
