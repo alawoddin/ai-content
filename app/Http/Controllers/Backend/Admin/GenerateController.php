59e528766614df738c4fcb5eb3bee4256a756b00
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-03-28 15:35:00 +0430

    the generate image table is create

--- app/Http/Controllers/Backend/Admin/GenerateController.php
+++ app/Http/Controllers/Backend/Admin/GenerateController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Backend\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\GeneratedImage;
+use OpenAI\Laravel\Facades\OpenAI;
 
 class GenerateController extends Controller
 {
@@ -13,4 +15,29 @@ class GenerateController extends Controller
 
     }
     // End Method
+
+     public function GenerateAndSaveImage(Request $request){
+
+        $request->validate([
+            'prompt' => 'required|string',
+        ]);
+
+        $prompt = $request->input('prompt');
+
+        /// Step 1: Generate image using OpenAI
+        $response = OpenAI::image()->create([
+            'model' => 'dall-e-3',
+            'prompt' => $prompt,
+            'n' => 1,
+            'size' => '1024x1024',
+        ]);
+
+        $imageUrl = $request->data[0]->url;
+
+        // Step 2: Download the image 
+        
+
+    }
+    // End Method
+
 }
