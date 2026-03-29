540a3bd0967555ee8c0e3c4db2500e3c8c651e93
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-03-29 15:52:08 +0430

    the admin same page style is fix

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,26 +1,27 @@
   <div class="nk-footer">
-                    <div class="container-xl">
-                        <div class="d-flex align-items-center flex-wrap justify-content-between mx-n3">
-                            <div class="nk-footer-links px-3">
-                                <ul class="nav nav-sm">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/#">Pricing</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/#">Privacy Policy</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/#">FAQ</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/#">Contact</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="nk-footer-copyright fs-6 px-3"> &copy; 2026 All Rights Reserved to <a href="#">Alawoddin</a>. </div>
-                        </div>
-                    </div>
-                </div>
+      <div class="container-xl">
+          <div class="d-flex align-items-center flex-wrap justify-content-between mx-n3">
+              <div class="nk-footer-links px-3">
+                  <ul class="nav nav-sm">
+                      <li class="nav-item">
+                          <a class="nav-link" href="/#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="/#">Pricing</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="/#">Privacy Policy</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="/#">FAQ</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="/#">Contact</a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="nk-footer-copyright fs-6 px-3"> &copy; 2026 All Rights Reserved to <a
+                      href="#">alawoddinkhan.com.</a>. </div>
+          </div>
+      </div>
+  </div>
